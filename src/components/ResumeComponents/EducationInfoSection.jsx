f0d6fc4f3e6b2ff8f6c414bf784f15677d037829
Alexandru Mosgoreanu <amosgoreanu@gmail.com>
2023-12-11 16:36:02 +0200

    Hide section title if all elements are hidden

--- src/components/ResumeComponents/EducationInfoSection.jsx
+++ src/components/ResumeComponents/EducationInfoSection.jsx
@@ -1,21 +1,25 @@
 import EducationInfoItem from './EducationInfoItem'
 
 const EducationInfoSection = ({ educationDetails }) => {
-    return (
-        <div className="infoSection">
-            <h3>Education</h3>
-            {educationDetails.map((item) =>  item.isHidden ? null :
-                (       
-                <EducationInfoItem
-                    key={item.key}
-                    degree={item.degree}
-                    university={item.university}
-                    location={item.location}
-                    startDate={item.startDate}
-                    endDate={item.endDate}
-                />
-                ))}          
-        </div>
+    return ( 
+        <>
+        {!educationDetails.every(item => item.isHidden) && (
+            <div className="infoSection">
+                <h3>Education</h3>
+                {educationDetails.map((item) =>  item.isHidden ? null :
+                    (       
+                    <EducationInfoItem
+                        key={item.key}
+                        degree={item.degree}
+                        university={item.university}
+                        location={item.location}
+                        startDate={item.startDate}
+                        endDate={item.endDate}
+                    />
+                    ))}          
+            </div>
+            )}
+        </>
     )
 }
 
--- src/components/ResumeComponents/ExperienceInfoSection.jsx
+++ src/components/ResumeComponents/ExperienceInfoSection.jsx
@@ -2,21 +2,24 @@ import ExperienceInfoItem from "./ExperienceInfoItem"
 
 const ExperienceInfoSection = ({ experienceDetails }) => {
     return (
-        <div className="infoSection">
-            <h3>Experience</h3>
-            {experienceDetails.map((item) =>  item.isHidden ? null :
-                (       
-                <ExperienceInfoItem
-                    key={item.key}
-                    company={item.company}
-                    position={item.position}
-                    location={item.location}
-                    startDate={item.startDate}
-                    endDate={item.endDate}
-                    description={item.description}
-                />
+        <>
+            {!experienceDetails.every(item => item.isHidden) &&
+            <div className="infoSection">
+                <h3>Experience</h3>
+                {experienceDetails.map((item) =>  item.isHidden ? null :
+                    (       
+                    <ExperienceInfoItem
+                        key={item.key}
+                        company={item.company}
+                        position={item.position}
+                        location={item.location}
+                        startDate={item.startDate}
+                        endDate={item.endDate}
+                        description={item.description}
+                    />
                 ))}          
-        </div>
+            </div> }
+        </>
     )
 }
 
